6fde6654f6c701b4794a7c1c30340aa8cfc41bd9
Nils <nilsxegger@gmail.com>
2021-06-25 15:25:25 +0200

    fixed bug which failed to set multiple fake values

--- libauth/tests/src/auth_fixture.cpp
+++ libauth/tests/src/auth_fixture.cpp
@@ -30,39 +30,51 @@
 #include "radicle/pgdb.h"
 
 /**
+ * @brief Creates column attributes from name and length array.
+ *
+ * @param result Result which will contain columns.
+ * @param descs Pointer to array to be filled.
+ * @param columns Size of \p descs
+ * @param names List of names of columns.
+ *
+ * @returns Returns 0 on success.
+ */
+int set_fake_columns_attributes(PGresult* result, PGresAttDesc* descs, const int columns, const char** names) {
+	for(int i = 0; i < columns; i++) {
+		descs[i] = {(char*)names[i], 0, 0, 1, 0, 0, 0};
+	}
+	if(PQsetResultAttrs(result, columns, descs) == 0)
+		return 1;
+	return 0;
+}
+
+/**
  * @brief Sets values of a fake PGresult. Should only be used for testing.
- * @bug Doesnt work for multiple values. Instead pass around PGresAttDesc pointer.
  *
  * @param result Result to be filled with values.
  * @param row Row of result to fill.
  * @param column Column of value to be inserted.
- * @param name Column name.
  * @param value Column value.
  * @param length Column value length.
  *
  * @returns Returns 0 on succes.
  */
-int set_fake_value(PGresult* result, const int row, const int column, const char* name, char* value, int length) {
-	PGresAttDesc attr = {(char*)name, 0, 0, 1, 0, length, 0};
-	// TODO for multiple list is needed ,
-	if(PQsetResultAttrs(result, 1, &attr) == 0) {
-		return 1;
-	}
+int set_fake_value(PGresult* result, const int row, const int column, char* value, const int length) {
 	if(PQsetvalue(result, row, column, value, length) == 0) {
 		return 1;
 	}
 	return 0;
 }
 
-int set_fake_uuid(PGresult* result, const int row, const int column, const char* name) {
+int set_fake_uuid(PGresult* result, const int row, const int column) {
 	char uuid[16] = {0x00};
-	return set_fake_value(result, row, column, name, uuid, 16);
+	return set_fake_value(result, row, column, uuid, 16);
 }
 
-int set_fake_int(PGresult* result, const int row, const int column, const char* name, int value) {
+int set_fake_int(PGresult* result, const int row, const int column, int value) {
 	uint32_t* buffer = (uint32_t*)malloc(sizeof(uint32_t));
 	*buffer = htonl((uint32_t)value);
-	if(set_fake_value(result, row, column, name, (char*)buffer, sizeof(uint32_t)))  {
+	if(set_fake_value(result, row, column, (char*)buffer, sizeof(uint32_t)))  {
 		free(buffer);
 		return 1;
 	}
@@ -70,13 +82,13 @@ int set_fake_int(PGresult* result, const int row, const int column, const char* 
 	return 0;
 }
 
-int set_fake_text(PGresult* result, const int row, const int column, const char* name) {
+int set_fake_text(PGresult* result, const int row, const int column) {
 	const char* txt = "I am a fake database string.!";
-	return set_fake_value(result, row, column, name, (char*) txt, strlen(txt));
+	return set_fake_value(result, row, column, (char*) txt, strlen(txt));
 }
 
-int set_fake_bool(PGresult* result, const int row, const int column, const char* name, bool value) {
-	return set_fake_value(result, row, column, name, (char*)&value, 1);
+int set_fake_bool(PGresult* result, const int row, const int column, bool value) {
+	return set_fake_value(result, row, column, (char*)&value, 1);
 }
 
 int pgdb_execute_param_fake(PGconn* conn, const char* stmt, const pgdb_params_t* params) {
@@ -85,23 +97,74 @@ int pgdb_execute_param_fake(PGconn* conn, const char* stmt, const pgdb_params_t*
 
 int pgdb_fetch_param_fake_uuid(PGconn* conn, const char* stmt, const pgdb_params_t* params, pgdb_result_t** result) {
 	*result = pgdb_result_new(PQmakeEmptyPGresult(conn, PGRES_TUPLES_OK));
-	return set_fake_uuid((*result)->pg, 0, 0, "uuid");	
+	PGresAttDesc descs;
+	const char* name = "uuid";
+	if(set_fake_columns_attributes((*result)->pg, &descs, 1, &name)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_uuid((*result)->pg, 0, 0)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	return 0;
 }
 
 int pgdb_fetch_param_fake_id(PGconn* conn, const char* stmt, const pgdb_params_t* params, pgdb_result_t** result) {
 	*result = pgdb_result_new(PQmakeEmptyPGresult(conn, PGRES_TUPLES_OK));
-	return set_fake_int((*result)->pg, 0, 0, "id", 1);	
+	PGresAttDesc descs;
+	const char* name = "id";
+	if(set_fake_columns_attributes((*result)->pg, &descs, 1, &name)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_int((*result)->pg, 0, 0, 1)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	return 0;
 }
 
 int pgdb_fetch_param_fake_complete(PGconn* conn, const char* stmt, const pgdb_params_t* params, pgdb_result_t** result) {
 	*result = pgdb_result_new(PQmakeEmptyPGresult(conn, PGRES_TUPLES_OK));
-	set_fake_uuid((*result)->pg, 0, 0, "uuid");
-	set_fake_text((*result)->pg, 0, 1, "password");
-	set_fake_text((*result)->pg, 0, 2, "role");
-	set_fake_bool((*result)->pg, 0, 3, "verified", true);
-	set_fake_bool((*result)->pg, 0, 4, "active", true);
-	set_fake_int((*result)->pg, 0, 5, "created", 1624020176);
-	set_fake_int((*result)->pg, 0, 6, "id", 1);
+	PGresAttDesc descs[8];
+	const char* columns[] = {"uuid", "email", "password", "role", "verified", "active", "created", "id"};
+	if(set_fake_columns_attributes((*result)->pg, descs, 8, columns)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_uuid((*result)->pg, 0, 0)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_text((*result)->pg, 0, 1)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_text((*result)->pg, 0, 2)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_text((*result)->pg, 0, 3)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_bool((*result)->pg, 0, 4, true)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_bool((*result)->pg, 0, 5, true)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_int((*result)->pg, 0, 6, 1624020176)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
+	if(set_fake_int((*result)->pg, 0, 7, 1)) {
+		PQclear((*result)->pg);
+		return 1;
+	}
 	return 0;
 }
 
